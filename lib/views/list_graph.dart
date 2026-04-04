fecc626f4c9319eddfbe01eb07743197a7aa6d94
Kairu <abandiakyle2112@gmail.com>
2026-04-04 12:43:40 +0800

    Fixed divider colors in overall statistics

--- lib/views/list_graph.dart
+++ lib/views/list_graph.dart
@@ -16,184 +16,197 @@ class _ListGraphState extends State<ListGraph> {
     return ValueListenableBuilder(
       valueListenable: category_expenses,
       builder: (context, category, child) {
-        return Padding(
-          padding: const EdgeInsets.only(left: 25, right: 25),
-          child: ValueListenableBuilder(
-            valueListenable: container_colors,
-            builder: (context, decorate, child) {
-              return Container(
-                height: 260,
-                decoration: decorate,
-                child: Padding(
-                  padding: const EdgeInsets.all(10),
-                  child: Column(
-                    children: [
-                      Container(
-                        color: Colors.transparent,
-                        height: 30,
-                        child: Row(
-                          children: [
-                            Icon(FontAwesomeIcons.bowlFood),
-                            VerticalDivider(
-                              thickness: 2,
-                              color: Colors.black,
-                              endIndent: 2,
-                              indent: 2,
+        return ValueListenableBuilder(
+          valueListenable: dark_mode,
+          builder: (context, dark, child) {
+            return Padding(
+              padding: const EdgeInsets.only(left: 25, right: 25),
+              child: ValueListenableBuilder(
+                valueListenable: container_colors,
+                builder: (context, decorate, child) {
+                  return Container(
+                    height: 260,
+                    decoration: decorate,
+                    child: Padding(
+                      padding: const EdgeInsets.all(10),
+                      child: Column(
+                        children: [
+                          Container(
+                            color: Colors.transparent,
+                            height: 30,
+                            child: Row(
+                              children: [
+                                Icon(FontAwesomeIcons.bowlFood),
+                                VerticalDivider(
+                                  thickness: 2,
+                                  color: dark ? Colors.white : Colors.black,
+                                  endIndent: 2,
+                                  indent: 2,
+                                ),
+                                Expanded(child: Text("Food")),
+                                Text(
+                                  "-₱${category["Food"]}",
+                                  style:
+                                      TextStyleDisplay.overall_balance_negative,
+                                ),
+                              ],
                             ),
-                            Expanded(child: Text("Food")),
-                            Text(
-                              "-₱${category["Food"]}",
-                              style: TextStyleDisplay.overall_balance_negative,
+                          ),
+                          Container(
+                            color: Colors.transparent,
+                            height: 30,
+                            child: Row(
+                              children: [
+                                Icon(FontAwesomeIcons.car),
+                                VerticalDivider(
+                                  thickness: 2,
+                                  color: dark ? Colors.white : Colors.black,
+                                  endIndent: 2,
+                                  indent: 2,
+                                ),
+                                Expanded(child: Text("Transport")),
+                                Text(
+                                  "-₱${category["Transport"]}",
+                                  style:
+                                      TextStyleDisplay.overall_balance_negative,
+                                ),
+                              ],
                             ),
-                          ],
-                        ),
+                          ),
+                          Container(
+                            color: Colors.transparent,
+                            height: 30,
+                            child: Row(
+                              children: [
+                                Icon(Icons.food_bank_outlined),
+                                VerticalDivider(
+                                  thickness: 2,
+                                  color: dark ? Colors.white : Colors.black,
+                                  endIndent: 2,
+                                  indent: 2,
+                                ),
+                                Expanded(child: Text("Groceries")),
+                                Text(
+                                  "-₱${category["Groceries"]}",
+                                  style:
+                                      TextStyleDisplay.overall_balance_negative,
+                                ),
+                              ],
+                            ),
+                          ),
+                          Container(
+                            color: Colors.transparent,
+                            height: 30,
+                            child: Row(
+                              children: [
+                                Icon(Icons.gamepad),
+                                VerticalDivider(
+                                  thickness: 2,
+                                  color: dark ? Colors.white : Colors.black,
+                                  endIndent: 2,
+                                  indent: 2,
+                                ),
+                                Expanded(child: Text("Entertainment")),
+                                Text(
+                                  "-₱${category["Entertainment"]}",
+                                  style:
+                                      TextStyleDisplay.overall_balance_negative,
+                                ),
+                              ],
+                            ),
+                          ),
+                          Container(
+                            color: Colors.transparent,
+                            height: 30,
+                            child: Row(
+                              children: [
+                                Icon(FontAwesomeIcons.book),
+                                VerticalDivider(
+                                  thickness: 2,
+                                  color: dark ? Colors.white : Colors.black,
+                                  endIndent: 2,
+                                  indent: 2,
+                                ),
+                                Expanded(child: Text("Education")),
+                                Text(
+                                  "-₱${category["Education"]}",
+                                  style:
+                                      TextStyleDisplay.overall_balance_negative,
+                                ),
+                              ],
+                            ),
+                          ),
+                          Container(
+                            color: Colors.transparent,
+                            height: 30,
+                            child: Row(
+                              children: [
+                                Icon(FontAwesomeIcons.moneyBill),
+                                VerticalDivider(
+                                  thickness: 2,
+                                  color: dark ? Colors.white : Colors.black,
+                                  endIndent: 2,
+                                  indent: 2,
+                                ),
+                                Expanded(child: Text("Bills")),
+                                Text(
+                                  "-₱${category["Bills"]}",
+                                  style:
+                                      TextStyleDisplay.overall_balance_negative,
+                                ),
+                              ],
+                            ),
+                          ),
+                          Container(
+                            color: Colors.transparent,
+                            height: 30,
+                            child: Row(
+                              children: [
+                                Icon(Icons.healing),
+                                VerticalDivider(
+                                  thickness: 2,
+                                  color: dark ? Colors.white : Colors.black,
+                                  endIndent: 2,
+                                  indent: 2,
+                                ),
+                                Expanded(child: Text("Health")),
+                                Text(
+                                  "-₱${category["Health"]}",
+                                  style:
+                                      TextStyleDisplay.overall_balance_negative,
+                                ),
+                              ],
+                            ),
+                          ),
+                          Container(
+                            color: Colors.transparent,
+                            height: 30,
+                            child: Row(
+                              children: [
+                                Icon(Icons.more_horiz_outlined),
+                                VerticalDivider(
+                                  thickness: 2,
+                                  color: dark ? Colors.white : Colors.black,
+                                  endIndent: 2,
+                                  indent: 2,
+                                ),
+                                Expanded(child: Text("Others")),
+                                Text(
+                                  "-₱${category["Others"]}",
+                                  style:
+                                      TextStyleDisplay.overall_balance_negative,
+                                ),
+                              ],
+                            ),
+                          ),
+                        ],
                       ),
-                      Container(
-                        color: Colors.transparent,
-                        height: 30,
-                        child: Row(
-                          children: [
-                            Icon(FontAwesomeIcons.car),
-                            VerticalDivider(
-                              thickness: 2,
-                              color: Colors.black,
-                              endIndent: 2,
-                              indent: 2,
-                            ),
-                            Expanded(child: Text("Transport")),
-                            Text(
-                              "-₱${category["Transport"]}",
-                              style: TextStyleDisplay.overall_balance_negative,
-                            ),
-                          ],
-                        ),
-                      ),
-                      Container(
-                        color: Colors.transparent,
-                        height: 30,
-                        child: Row(
-                          children: [
-                            Icon(Icons.food_bank_outlined),
-                            VerticalDivider(
-                              thickness: 2,
-                              color: Colors.black,
-                              endIndent: 2,
-                              indent: 2,
-                            ),
-                            Expanded(child: Text("Groceries")),
-                            Text(
-                              "-₱${category["Groceries"]}",
-                              style: TextStyleDisplay.overall_balance_negative,
-                            ),
-                          ],
-                        ),
-                      ),
-                      Container(
-                        color: Colors.transparent,
-                        height: 30,
-                        child: Row(
-                          children: [
-                            Icon(Icons.gamepad),
-                            VerticalDivider(
-                              thickness: 2,
-                              color: Colors.black,
-                              endIndent: 2,
-                              indent: 2,
-                            ),
-                            Expanded(child: Text("Entertainment")),
-                            Text(
-                              "-₱${category["Entertainment"]}",
-                              style: TextStyleDisplay.overall_balance_negative,
-                            ),
-                          ],
-                        ),
-                      ),
-                      Container(
-                        color: Colors.transparent,
-                        height: 30,
-                        child: Row(
-                          children: [
-                            Icon(FontAwesomeIcons.book),
-                            VerticalDivider(
-                              thickness: 2,
-                              color: Colors.black,
-                              endIndent: 2,
-                              indent: 2,
-                            ),
-                            Expanded(child: Text("Education")),
-                            Text(
-                              "-₱${category["Education"]}",
-                              style: TextStyleDisplay.overall_balance_negative,
-                            ),
-                          ],
-                        ),
-                      ),
-                      Container(
-                        color: Colors.transparent,
-                        height: 30,
-                        child: Row(
-                          children: [
-                            Icon(FontAwesomeIcons.moneyBill),
-                            VerticalDivider(
-                              thickness: 2,
-                              color: Colors.black,
-                              endIndent: 2,
-                              indent: 2,
-                            ),
-                            Expanded(child: Text("Bills")),
-                            Text(
-                              "-₱${category["Bills"]}",
-                              style: TextStyleDisplay.overall_balance_negative,
-                            ),
-                          ],
-                        ),
-                      ),
-                      Container(
-                        color: Colors.transparent,
-                        height: 30,
-                        child: Row(
-                          children: [
-                            Icon(Icons.healing),
-                            VerticalDivider(
-                              thickness: 2,
-                              color: Colors.black,
-                              endIndent: 2,
-                              indent: 2,
-                            ),
-                            Expanded(child: Text("Health")),
-                            Text(
-                              "-₱${category["Health"]}",
-                              style: TextStyleDisplay.overall_balance_negative,
-                            ),
-                          ],
-                        ),
-                      ),
-                      Container(
-                        color: Colors.transparent,
-                        height: 30,
-                        child: Row(
-                          children: [
-                            Icon(Icons.more_horiz_outlined),
-                            VerticalDivider(
-                              thickness: 2,
-                              color: Colors.black,
-                              endIndent: 2,
-                              indent: 2,
-                            ),
-                            Expanded(child: Text("Others")),
-                            Text(
-                              "-₱${category["Others"]}",
-                              style: TextStyleDisplay.overall_balance_negative,
-                            ),
-                          ],
-                        ),
-                      ),
-                    ],
-                  ),
-                ),
-              );
-            },
-          ),
+                    ),
+                  );
+                },
+              ),
+            );
+          },
         );
       },
     );
